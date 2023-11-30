b528ca1a075f7bc00c7eba4de3e04da18ed9a911
Bloomberger <123385187+Bloomberger@users.noreply.github.com>
2023-11-30 19:30:24 +0200

    Fixed change_data_secure.sql

--- Laboratory-work-8/change_data_secure.sql
+++ Laboratory-work-8/change_data_secure.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE FUNCTION change_data_secure (pr_1 varchar, pr_2 varchar)
 RETURNS varchar
-as $$
-declare 
-	str varchar;
-begin
-	str:= 'UPDATE office SET name = $2 WHERE name = $1';
-	raise notice 'Query=%', str;
-	execute str using pr_1, pr_2;
-	return 'Office name was updated';
-end;
+AS $$
+DECLARE
+    str varchar;
+BEGIN
+    str := 'UPDATE office SET name = $2 WHERE name = $1';
+    raise notice 'Query=%', str;
+    execute str using pr_1, pr_2;
+    return 'Office name was updated';
+END;
 $$ LANGUAGE plpgsql;
