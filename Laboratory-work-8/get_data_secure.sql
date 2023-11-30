6731aabaefd9d72aff3b1654f48ce0fafce10193
Bloomberger <123385187+Bloomberger@users.noreply.github.com>
2023-11-30 19:21:11 +0200

    Created get_data_secure.sql

---
+++ Laboratory-work-8/get_data_secure.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION get_data_secure(class_office_name varchar)
+RETURNS TABLE(o_id integer, name varchar, size varchar, spot_conf integer)
+AS $$
+declare
+  str varchar;
+begin
+	str:= 'SELECT * FROM office WHERE name::varchar = $1';
+	raise notice 'Query = %', str;
+	return query execute str using class_pupil_name;
+end;
+$$ LANGUAGE plpgsql;
