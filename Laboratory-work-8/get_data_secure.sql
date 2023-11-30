a80eb5fba61113d44df71e680351f9613a0c9de7
Bloomberger <123385187+Bloomberger@users.noreply.github.com>
2023-11-30 19:39:22 +0200

    Fixed get_data_secure.sql

--- Laboratory-work-8/get_data_secure.sql
+++ Laboratory-work-8/get_data_secure.sql
@@ -4,7 +4,7 @@ AS $$
 DECLARE
     str varchar;
 BEGIN
-    str := 'SELECT o_id::integer, name::varchar, size::varchar, spot_conf:integer FROM office WHERE name::varchar = $1';
+    str := 'SELECT o_id::integer, name::varchar, size::varchar, spot_conf::integer FROM office WHERE name::varchar = $1';
     raise notice 'Query = %', str;
     return query execute str using class_office_name;
 END;
