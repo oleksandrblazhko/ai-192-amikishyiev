aea0046ccd8c868a22795773ac99a7e8343992dd
Bloomberger <123385187+Bloomberger@users.noreply.github.com>
2023-11-30 19:35:28 +0200

    Updated get_data_secure.sql

--- Laboratory-work-8/get_data_secure.sql
+++ Laboratory-work-8/get_data_secure.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE FUNCTION get_data_secure(class_office_name varchar)
 RETURNS TABLE(o_id integer, name varchar, size varchar, spot_conf integer)
 AS $$
-declare
-  str varchar;
-begin
-	str:= 'SELECT * FROM office WHERE name = $1';
-	raise notice 'Query = %', str;
-	return query execute str using class_office_name;
-end;
+DECLARE
+    str varchar;
+BEGIN
+    str := 'SELECT o_id, name, size, spot_conf FROM office WHERE name = $1';
+    raise notice 'Query = %', str;
+    return query execute str using class_office_name;
+END;
 $$ LANGUAGE plpgsql;
